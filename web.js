0d8d22c918f893c87e95cd709b54a63f732552a9
Ubuntu <ubuntu@ip-172-31-38-194.us-west-2.compute.internal>
2013-07-17 11:34:41 +0000

    changed the deprecated express api

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
-var express = require('express');
+var express = require("express");
+var app = express();
+//var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
 var fs = require ('fs');
 var text = fs.readFileSync('index.html','utf8');
@@ -9,7 +11,7 @@ app.get('/', function(request, response) {
   response.send(text);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
